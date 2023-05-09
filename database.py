72b8ba99f7340317b9057f688628c502c9f25edd
Trymne <tsnps3@gmail.com>
2023-05-09 19:15:30 +0200

    updated db connection

--- database.py
+++ database.py
@@ -2,23 +2,23 @@ from pymongo import MongoClient
 from dotenv import dotenv_values
 import os
 
-if os.path.exists('.env'):
-    # Read from the .env file
-    config = dotenv_values('.env')
-    MONGO_URI = config['MONGO_URI']
-    client = MongoClient(MONGO_URI)
+# if os.path.exists('.env'):
+# Read from the .env file
+config = dotenv_values('.env')
+MONGO_URI = config['MONGO_URI']
+client = MongoClient(MONGO_URI)
 
-else:
-    # Use environmental variables
-    MONGO_URI = os.environ.get('MONGO_URI')
+# else:
+#     # Use environmental variables
+#     MONGO_URI = os.environ.get('MONGO_URI')
 
-    host = os.environ.get('MONGOHOST')
-    pw = os.environ.get('MONGOPASSWORD')
-    port = int(os.environ.get('MONGOPORT'))  # type: ignore
-    print(type(port), port)
-    user = os.environ.get('MONGOUSER')
+#     host = os.environ.get('MONGOHOST')
+#     pw = os.environ.get('MONGOPASSWORD')
+#     port = int(os.environ.get('MONGOPORT'))  # type: ignore
+#     print(type(port), port)
+#     user = os.environ.get('MONGOUSER')
 
-    client = MongoClient(host, port, password=pw, username=user)
+#     client = MongoClient(host, port, password=pw, username=user)
 
 # connecting to the database
 db = client['workshop_db']
