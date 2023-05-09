670b206dc6e47849d52a486d797f74475a9f39ad
Trymne <tsnps3@gmail.com>
2023-05-09 19:24:25 +0200

    we try to connect again

--- database.py
+++ database.py
@@ -2,23 +2,17 @@ from pymongo import MongoClient
 from dotenv import dotenv_values
 import os
 
-# if os.path.exists('.env'):
-# Read from the .env file
-config = dotenv_values('.env')
-MONGO_URI = config['MONGO_URI']
+if os.path.exists('.env'):
+    # Read from the .env file
+    config = dotenv_values('.env')
+    MONGO_URI = config['MONGO_URI']
+    client = MongoClient(MONGO_URI)
+
+else:
+    # Use environmental variables
+    MONGO_URI = os.environ.get('MONGO_URI')
+
 client = MongoClient(MONGO_URI)
-
-# else:
-#     # Use environmental variables
-#     MONGO_URI = os.environ.get('MONGO_URI')
-
-#     host = os.environ.get('MONGOHOST')
-#     pw = os.environ.get('MONGOPASSWORD')
-#     port = int(os.environ.get('MONGOPORT'))  # type: ignore
-#     print(type(port), port)
-#     user = os.environ.get('MONGOUSER')
-
-#     client = MongoClient(host, port, password=pw, username=user)
 
 # connecting to the database
 db = client['workshop_db']
@@ -29,6 +23,3 @@ try:
     print('Connected to the database')
 except Exception as e:
     print(f'Error connecting to the database: {str(e)}')
-    exit()
-
-print('Database connection established successfully.')
